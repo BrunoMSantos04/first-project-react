f803202b1b639e70e2fd75481caa32099351d459
Bruno Santos <marcilio.santos@aluno.ifsp.edu.br>
2023-10-06 18:01:53 -0300

    implementação do globalstyle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  H1,
+  Img,
+  Itens,
+  LabelInput,
+  Input,
+  Button
+} from './styles';
 
+//JSX
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+return (
+  <Container>
+      <Img />
+      <Itens>
+        <H1>Olá</H1>
+
+        <LabelInput>Nome</LabelInput>
+        <input placeholder="Nome" />
+
+        <LabelInput>Idade</LabelInput>
+        <Input placeholder="Idade" />
+
+        <Button>
+            Cadastrar 
+        </Button>
+      </Itens>
+  </Container>
   );
 }
 
-export default App;
+export default App
---
+++ src/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+import bg1 from './assets/bg image1.svg'
+import bg2 from './assets/bg image2.svg'
+
+
+
+export const Container = styled.div`
+    background: url("${bg1}");
+    background-size: cover;
+`;
+
+export const Img = styled.img`
+    
+`;
+
+export const Itens = styled.div`
+    
+`;
+
+export const H1 = styled.h1`
+
+`;
+
+export const LabelInput = styled.p`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
